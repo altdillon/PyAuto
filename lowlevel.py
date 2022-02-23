bd08043fcf3b6e468c9579c0b32f5422a2f68143
Tyler Couvrette <couvrettetyler@gmail.com>
2022-02-22 16:46:53 -0800

    completed bug fixes and optimized import speed. Next step is instrument drivers

--- lowlevel.py
+++ lowlevel.py
@@ -1,17 +1,20 @@
 import re
 
-def instantiate_inst(class_name, inst):
-	'''creates instrument and passes it back as a list'''
-	eval(f'from {class_name} import {class_name}')
+def instantiate_inst(gid, inst, connection_mode):
+    '''creates instrument and passes it back as a list'''
+    exec(f'from cInst_{gid} import cInst_{gid}')
 
-	connection_mode = re.sub(r'\d+','',str(inst))[:5].strip("In")
-	if connection_mode == "USB":
-		address = (str(inst).split("::")[-2])
-	else:
-		address = (str(inst).split("::")[1])
-        
-	inst_to_add = eval(f'{class_name}(temp, class_name[6:], connection_mode, address)') #this will return either a single object or a list of multiple objects
-	if type(inst_to_add) is list:
-		return inst_to_add
-	else:
-		return [inst_to_add]
+    if connection_mode == "USB":
+        inst_str = str(inst).split("::")
+        if len(inst_str) > 1:
+            address = inst_str[-2]
+        else:
+            address = inst_str[0]
+    else:
+        address = str(inst).split("::")[1]
+
+    inst_to_add = eval(f'cInst_{gid}(inst, gid, connection_mode, address)') #this will return either a single object or a list of multiple objects
+    if type(inst_to_add) is list:
+        return inst_to_add
+    else:
+        return [inst_to_add]
