0da2abdd299916ed4dc93213b6f550beaedcd1c4
Tyler Couvrette <tyler@5thgait.com>
2022-04-13 15:12:24 -0600

    Started on the set_measurement funtion

--- inst/oscilloscope/cInst_MSO58LP.py
+++ inst/oscilloscope/cInst_MSO58LP.py
@@ -5,9 +5,14 @@ class cInst_MSO58LP(cInst_oscilloscope):
     '''
     TBD
     '''
+    def set_measurement(self, ...):
+        '''
+        See page 500 ish in the 5 series manual
+        '''
+        
     def get_plot(self, filename, invert = False):
         '''
-        Gets plot from scope and save it to the filename (full path) as a png passed to the function
+        Gets plot from scope and saves it to the filename (full path) as a png passed to the function
         '''
         if invert:
             self.comm("SAV:IMAG:COMP INVE")
@@ -30,13 +35,9 @@ class cInst_MSO58LP(cInst_oscilloscope):
                 raw = self.inst.read_raw()
             except:
                 print("Failed to aquire data from device. Saved this to file and continued test")
-                raw = 0b0100100010110101#"0200Failed to aquire data from device. Saved this to file and continued test"
+                raw = "Failed to aquire data from device. Saved this to file and continued test"
 
         self.inst.timeout = timeout
-        print(raw)
 
-        #leading_characters = int(chr(raw[1]))
-        leading_characters = 0
-
-        with open(filename,'wb') as fp:
-            fp.write(raw[2+leading_characters:])
+        with open(filename,'w') as fp:
+            fp.write(raw)
